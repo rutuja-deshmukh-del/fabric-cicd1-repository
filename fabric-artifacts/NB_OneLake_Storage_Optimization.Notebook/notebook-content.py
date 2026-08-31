3ba6d161bdb1414242f96cfaf44dca5b7fac7963
rutuja-deshmukh-del <rutujadeshmukh0704@gmail.com>
2026-08-31 14:21:54 +0530

    Committing 3 items from workspace 17f9540b-2901-4593-9d80-f65a9ac92f93

---
+++ fabric-artifacts/NB_OneLake_Storage_Optimization.Notebook/notebook-content.py
@@ -0,0 +1,38 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "environment": {
+# META       "environmentId": "11332024-11c9-96df-49e4-f27722a580ef",
+# META       "workspaceId": "00000000-0000-0000-0000-000000000000"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+spark.version
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
